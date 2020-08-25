19ec26ec0565f16d360ddd36cb121bab5a921364
William Pittman <wspittman@gmail.com>
2020-08-25 10:25:53 -0700

    Boilerplate

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>Skeleton</p>
+      </div>
+    );
+  }
+}
+
+export default App;
